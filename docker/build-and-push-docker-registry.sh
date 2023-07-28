fe786e19a3613085adfe4f3960b1c574dc097cc5
kimhyunki(falinux) <khkraining@falinux.com>
2023-07-28 17:55:57 +0900

    test: script instruction echo

--- docker/build-and-push-docker-registry.sh
+++ docker/build-and-push-docker-registry.sh
@@ -18,12 +18,12 @@ echo "Docker image build and push registry ..."
 echo ""
 
 # docker-compose build --no-cache falinux-services-supercom-backend
-docker-compose build falinux-services-supercom-backend
-
-docker tag ${DOCKER_PATH}/${DOCKER_IMG_NAME}:${DOCKER_IMG_TAG} ${DOCKER_REG}/${DOCKER_PATH}/${DOCKER_IMG_NAME}:${DOCKER_IMG_TAG}
-docker login -u ${DOCKER_REG_USER} -p ${DOCKER_REG_PASSWORD} ${DOCKER_REG}
-docker tag ${DOCKER_PATH}/${DOCKER_IMG_NAME} ${DOCKER_REG}/${DOCKER_PATH}/${DOCKER_IMG_NAME}:${DOCKER_IMG_TAG}
-docker push ${DOCKER_REG}/${DOCKER_PATH}/${DOCKER_IMG_NAME}:${DOCKER_IMG_TAG}
-docker logout ${DOCKER_REG}
-
-kubectl --kubeconfig ~/.kube/config-clone -n supercom rollout restart deployment/backend
+echo "docker-compose build falinux-services-supercom-backend"
+echo ""
+echo "docker tag ${DOCKER_PATH}/${DOCKER_IMG_NAME}:${DOCKER_IMG_TAG} ${DOCKER_REG}/${DOCKER_PATH}/${DOCKER_IMG_NAME}:${DOCKER_IMG_TAG}"
+echo "docker login -u ${DOCKER_REG_USER} -p ${DOCKER_REG_PASSWORD} ${DOCKER_REG}"
+echo "docker tag ${DOCKER_PATH}/${DOCKER_IMG_NAME} ${DOCKER_REG}/${DOCKER_PATH}/${DOCKER_IMG_NAME}:${DOCKER_IMG_TAG}"
+echo "docker push ${DOCKER_REG}/${DOCKER_PATH}/${DOCKER_IMG_NAME}:${DOCKER_IMG_TAG}"
+echo "docker logout ${DOCKER_REG}"
+echo ""
+echo "kubectl --kubeconfig ~/.kube/config-clone -n supercom rollout restart deployment/backend"
